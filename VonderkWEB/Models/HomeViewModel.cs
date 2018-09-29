a38a884a698bbbec221a1b352d43fad20c05ed4c
matiasmarangone <matiasemarangone@gmail.com>
2018-09-29 01:49:41 -0300

    Added views, Home viewmodel, fixed controllers

---
+++ VonderkWEB/Models/HomeViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VonderkWEB.Models
+{
+    public class HomeViewModel
+    {
+
+
+        private IEnumerable<Product> products;
+        private IEnumerable<Category> categories;
+        private IEnumerable<Slide> slides;
+        //private IEnumerable<Brand> brands;
+        //private List<string> orderedImages;
+
+
+
+        public IEnumerable<Product> Products { get { return this.products; } }
+        public IEnumerable<Category> Categories { get { return this.categories; } }
+        public IEnumerable<Slide> Slides { get { return this.slides; } }
+        //public IEnumerable<Brand> Brands { get { return this.brands; } }
+
+        private LuminariaEntities db = new LuminariaEntities();
+
+        public HomeViewModel()
+        {
+
+        }
+
+        public HomeViewModel Get()
+        {
+            return Get(String.Empty, 0, 0);
+        }
+
+        public HomeViewModel Get(string text = "", int categoryID = 0, int brandID = 0)
+        {
+            return new HomeViewModel
+            {
+                categories = db.Categories.Where(x => x.IsActive && (categoryID == 0 || x.CategoryID == categoryID)),
+                products = db.Products.Where(x => x.IsActive && (text == String.Empty || (x.Description.Contains(text) || x.Features.Contains(text) || x.Name.Contains(text)))),
+                slides = db.Slides.Where(x => x.IsActive),
+            };
+        }
+
+    }
+}
--- VonderkWEB/Models/ProductDetailsViewModel.cs
+++ VonderkWEB/Models/ProductDetailsViewModel.cs
@@ -9,15 +9,15 @@ namespace VonderkWEB.Models
     {
         private Product product;
         private IEnumerable<Product> relatedProducts;
-        private List<string> orderedImages;
-        private List<ProductAsset> orderedFichas;
-        private List<ProductAsset> orderedIES;
+        private IEnumerable<ProductAsset> orderedImages;
+        private IEnumerable<ProductAsset> orderedFichas;
+        private IEnumerable<ProductAsset> orderedIES;
 
         public Product Product { get { return this.product; } }
         public IEnumerable<Product> RelatedProducts { get { return this.relatedProducts; } }
-        public List<string> OrderedImages { get { return this.orderedImages; } }
-        public List<ProductAsset> OrderedFichas { get { return this.orderedFichas; } }
-        public List<ProductAsset> OrderedIES { get { return this.orderedIES; } }
+        public IEnumerable<ProductAsset> OrderedImages { get { return this.orderedImages; } }
+        public IEnumerable<ProductAsset> OrderedFichas { get { return this.orderedFichas; } }
+        public IEnumerable<ProductAsset> OrderedIES { get { return this.orderedIES; } }
 
         private LuminariaEntities db = new LuminariaEntities();
 
@@ -25,28 +25,9 @@ namespace VonderkWEB.Models
         {
             this.product = db.Products.First(x => x.ProductID == ProductID);
             this.relatedProducts = db.Products.Where(x => x.CategoryID == db.Products.FirstOrDefault(m => m.ProductID == ProductID).CategoryID);
-
-            this.orderedImages = new List<string>();
-            var order = db.ProductAssets.Where(x => x.ProductID == ProductID).OrderBy(x => x.SortOrder);
-            foreach (var item in order)
-            {
-                this.orderedImages.Add(item.Name);
-            }
-
-            this.orderedFichas = new List<ProductAsset>();
-            var orderFichas = db.ProductAssets.Where(x => x.ProductID == ProductID).OrderBy(x => x.SortOrder);
-            foreach (var item in orderFichas)
-            {
-                this.orderedFichas.Add(item);
-            }
-
-            this.orderedIES = new List<ProductAsset>();
-            var orderedIES = db.ProductAssets.Where(x => x.ProductID == ProductID).OrderBy(x => x.SortOrder);
-            foreach (var item in orderedIES)
-            {
-                this.orderedIES.Add(item);
-            }
-
+            this.orderedImages = db.ProductAssets.Where(x => x.ProductID == ProductID && x.AssetType == "IMG").OrderBy(x => x.SortOrder);
+            this.orderedFichas = db.ProductAssets.Where(x => x.ProductID == ProductID && x.AssetType == "PDF").OrderBy(x => x.SortOrder);
+            this.orderedIES = db.ProductAssets.Where(x => x.ProductID == ProductID && x.AssetType == "IES").OrderBy(x => x.SortOrder);
         }
 
 
