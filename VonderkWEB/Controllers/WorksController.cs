836655dafc48ac08bad6f70b75142eaf37576039
Matías Marangone <matiasmarangone@hotmail.com>
2018-10-03 16:32:34 -0300

    Created work details viewmodel, fixed view

--- VonderkWEB/Controllers/WorksController.cs
+++ VonderkWEB/Controllers/WorksController.cs
@@ -32,179 +32,186 @@ namespace VonderkWEB.Controllers
         [AllowAnonymous]
         public ActionResult Details(int? id)
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Work work = db.Works.Find(id);
-            if (work == null)
-            {
-                return HttpNotFound();
-            }
-            return View(work);
+
+            if (!id.HasValue) { return HttpNotFound(); }
+            WorkDetailsViewModel model = new WorkDetailsViewModel(id.Value);
+
+            return View(model);
         }
 
-        // GET: Works/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        //// POST: Works/Create
-        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        //// more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult Create([Bind(Include = "WorkID,Name,Description,IsActive")] Work work)
+        //if (id == null)
         //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        db.Works.Add(work);
-        //        db.SaveChanges();
-        //        return RedirectToAction("Index");
-        //    }
-
-        //    return View(work);
+        //    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
         //}
+        //Work work = db.Works.Find(id);
+        //if (work == null)
+        //{
+        //    return HttpNotFound();
+        //}
+        //return View(work);
+    
 
-        // POST: Trabajos/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(Work trabajo, List<HttpPostedFileBase> postedFiles)
+    // GET: Works/Create
+    public ActionResult Create()
+    {
+        return View();
+    }
+
+    //// POST: Works/Create
+    //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+    //// more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+    //[HttpPost]
+    //[ValidateAntiForgeryToken]
+    //public ActionResult Create([Bind(Include = "WorkID,Name,Description,IsActive")] Work work)
+    //{
+    //    if (ModelState.IsValid)
+    //    {
+    //        db.Works.Add(work);
+    //        db.SaveChanges();
+    //        return RedirectToAction("Index");
+    //    }
+
+    //    return View(work);
+    //}
+
+    // POST: Trabajos/Create
+    // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+    // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Create(Work trabajo, List<HttpPostedFileBase> postedFiles)
+    {
+
+        var errors = ModelState.Where(x => x.Value.Errors.Count > 0).Select(x => new { x.Key, x.Value.Errors }).ToArray();
+
+        if (ModelState.IsValid)
         {
 
-            var errors = ModelState.Where(x => x.Value.Errors.Count > 0).Select(x => new { x.Key, x.Value.Errors }).ToArray();
+            if (trabajo.Name == null)
+            {
+                throw new Exception("You have not specified a Name.");
 
-            if (ModelState.IsValid)
+            }
+            trabajo.IsActive = true;
+            db.Works.Add(trabajo);
+            db.SaveChanges();
+
+            if (postedFiles != null)
             {
 
-                if (trabajo.Name == null)
-                {    
-                    throw new Exception("You have not specified a Name.");
+                string buildPath = "~/Images/Works/" + trabajo.WorkID + "/";
+                string path = Server.MapPath(buildPath);
 
-                }
-                trabajo.IsActive = true;
-                db.Works.Add(trabajo);
-                db.SaveChanges();
-
-                if (postedFiles != null)
+                if (!Directory.Exists(path))
                 {
-
-                    string buildPath = "~/Images/Works/" + trabajo.WorkID + "/";
-                    string path = Server.MapPath(buildPath);
-
-                    if (!Directory.Exists(path))
-                    {
-                        Directory.CreateDirectory(path);
-                    }
-
-                    short cont = 0;
-                    foreach (HttpPostedFileBase postedFile in postedFiles)
-                    {
-                        WorkAsset asset = new WorkAsset
-                        {
-                            
-                            FileName = postedFile.FileName,
-                            AssetType = "IMG",
-                            SortOrder = cont,
-                            IsActive = true,
-                            Name = trabajo.Name,
-                            WorkID = trabajo.WorkID
-                           
-                        };
-                        db.WorkAssets.Add(asset);
-
-                        var pathFinal = postedFile.FileName;
-                        postedFile.SaveAs(path + pathFinal);
-
-                        cont++;
-
-                    }
-
-                    db.SaveChanges();
+                    Directory.CreateDirectory(path);
                 }
 
-                return RedirectToAction("Index");
-            }
+                short cont = 0;
+                foreach (HttpPostedFileBase postedFile in postedFiles)
+                {
+                    WorkAsset asset = new WorkAsset
+                    {
 
-            return View(trabajo);
-        }
+                        FileName = postedFile.FileName,
+                        AssetType = "IMG",
+                        SortOrder = cont,
+                        IsActive = true,
+                        Name = trabajo.Name,
+                        WorkID = trabajo.WorkID
 
-        // GET: Works/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Work work = db.Works.Find(id);
-            if (work == null)
-            {
-                return HttpNotFound();
-            }
-            return View(work);
-        }
+                    };
+                    db.WorkAssets.Add(asset);
 
-        // POST: Works/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "WorkID,Name,Description,IsActive")] Work work)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(work).State = EntityState.Modified;
+                    var pathFinal = postedFile.FileName;
+                    postedFile.SaveAs(path + pathFinal);
+
+                    cont++;
+
+                }
+
                 db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(work);
-        }
-
-        // GET: Works/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Work work = db.Works.Find(id);
-            if (work == null)
-            {
-                return HttpNotFound();
-            }
-            return View(work);
-        }
-
-        // POST: Works/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-
-            var findImages = db.WorkAssets.Where(x => x.WorkID == id);
-            foreach (var item in findImages)
-            {
-                db.WorkAssets.Remove(item);
             }
 
-            db.SaveChanges();
-
-            Work work = db.Works.Find(id);
-            db.Works.Remove(work);
-            db.SaveChanges();
             return RedirectToAction("Index");
         }
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+        return View(trabajo);
     }
+
+    // GET: Works/Edit/5
+    public ActionResult Edit(int? id)
+    {
+        if (id == null)
+        {
+            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        }
+        Work work = db.Works.Find(id);
+        if (work == null)
+        {
+            return HttpNotFound();
+        }
+        return View(work);
+    }
+
+    // POST: Works/Edit/5
+    // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+    // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Edit([Bind(Include = "WorkID,Name,Description,IsActive")] Work work)
+    {
+        if (ModelState.IsValid)
+        {
+            db.Entry(work).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        return View(work);
+    }
+
+    // GET: Works/Delete/5
+    public ActionResult Delete(int? id)
+    {
+        if (id == null)
+        {
+            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        }
+        Work work = db.Works.Find(id);
+        if (work == null)
+        {
+            return HttpNotFound();
+        }
+        return View(work);
+    }
+
+    // POST: Works/Delete/5
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public ActionResult DeleteConfirmed(int id)
+    {
+
+        var findImages = db.WorkAssets.Where(x => x.WorkID == id);
+        foreach (var item in findImages)
+        {
+            db.WorkAssets.Remove(item);
+        }
+
+        db.SaveChanges();
+
+        Work work = db.Works.Find(id);
+        db.Works.Remove(work);
+        db.SaveChanges();
+        return RedirectToAction("Index");
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing)
+        {
+            db.Dispose();
+        }
+        base.Dispose(disposing);
+    }
+}
 }
---
+++ VonderkWEB/Models/WorkDetailsViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VonderkWEB.Models
+{
+    public class WorkDetailsViewModel
+    {
+        private Work work;
+        private IEnumerable<Work> relatedWorks;
+
+        public Work Work { get { return this.work; } }
+        public IEnumerable<Work> RelatedWorks { get { return this.relatedWorks; } }
+
+        private LuminariaEntities db = new LuminariaEntities();
+        public WorkDetailsViewModel()
+        { }
+
+        public WorkDetailsViewModel(int WorkID)
+        {
+            this.work = db.Works.First(x => x.WorkID == WorkID);
+            this.relatedWorks = db.Works.Where(x => x.IsActive == true);
+
+        }
+
+
+    }
+}
