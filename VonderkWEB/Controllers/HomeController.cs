e0fe397885caebe0a1456771738155122ce6c2f6
Matías Marangone <matiasmarangone@hotmail.com>
2018-11-28 16:05:54 -0300

    fixed contact email send controller

--- VonderkWEB/Controllers/HomeController.cs
+++ VonderkWEB/Controllers/HomeController.cs
@@ -43,32 +43,44 @@ namespace VonderkWEB.Controllers
             {
                 var body = "<p><b>Emisor:</b> {0} ( {2} )( {1} )</p><p><b>Mensaje:</b></p><p>{4}</p><p><b>Compañia:</b></p><p>{3}</p>";
                 var message = new MailMessage();
-                message.To.Add(new MailAddress("matiasemarangone@gmail.com"));  // replace with valid value 
+                message.To.Add(new MailAddress("info@proyectosweb.site"));  // replace with valid value 
                 message.From = new MailAddress("info@proyectosweb.site");  // replace with valid value
                 message.Subject = "Contacto - VK Site";
                 message.Body = string.Format(body, model.FromName, model.FromPhone, model.FromEmail, model.FromCompany, model.Message);
                 message.IsBodyHtml = true;
 
-                using (var smtp = new SmtpClient())
-                {
-                    smtp.UseDefaultCredentials = true;
-                    //var credential = new NetworkCredential
-                    //{
-                    //    UserName = "",  // replace with valid value
-                    //    Password = ""  // replace with valid value
-                    //};
-                    //smtp.Credentials = credential;
-                    smtp.Host = "localhost";
-                    smtp.Port = 465;
-                    smtp.EnableSsl = false;
-
-                    await smtp.SendMailAsync(message);
+             
+                    using (var smtp = new SmtpClient())
+                    {
+                        smtp.Host = "localhost";
+                        smtp.Port = 25;
+                        smtp.EnableSsl = false;
+                        smtp.Timeout = 20000; //20sec
+                        
+                        await smtp.SendMailAsync(message);
                     return RedirectToAction("Index");
                 }
 
+               
 
+             
 
+                //using (var smtp = new SmtpClient())
+                //{
+                //    smtp.UseDefaultCredentials = true;
+                //    //var credential = new NetworkCredential
+                //    //{
+                //    //    UserName = "",  // replace with valid value
+                //    //    Password = ""  // replace with valid value
+                //    //};
+                //    //smtp.Credentials = credential;
+                //    smtp.Host = "localhost";
+                //    smtp.Port = 465;
+                //    smtp.EnableSsl = false;
 
+                //    await smtp.SendMailAsync(message);
+                //    return RedirectToAction("Index");
+                //}
 
             }
             return View(model);
