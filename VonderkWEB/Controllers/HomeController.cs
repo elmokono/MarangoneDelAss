f332a65ee1041cd1f3faf6611d7a9b66896cc80b
Matías Marangone <matiasmarangone@hotmail.com>
2018-11-28 14:35:59 -0300

    ok

--- VonderkWEB/Controllers/HomeController.cs
+++ VonderkWEB/Controllers/HomeController.cs
@@ -7,6 +7,8 @@ using VonderkWEB.Models;
 using System.Net;
 using System.Net.Mail;
 using System.Threading.Tasks;
+using System.Security.Cryptography.X509Certificates;
+using System.Net.Security;
 
 namespace VonderkWEB.Controllers
 {
@@ -20,14 +22,14 @@ namespace VonderkWEB.Controllers
 
         public ActionResult Empresa()
         {
-           
+
 
             return View();
         }
 
         public ActionResult Contacto()
         {
-           
+
 
             return View();
         }
@@ -42,27 +44,32 @@ namespace VonderkWEB.Controllers
                 var body = "<p><b>Emisor:</b> {0} ( {2} )( {1} )</p><p><b>Mensaje:</b></p><p>{4}</p><p><b>Compañia:</b></p><p>{3}</p>";
                 var message = new MailMessage();
                 message.To.Add(new MailAddress("matiasemarangone@gmail.com"));  // replace with valid value 
-                message.From = new MailAddress("matiasemarangone@gmail.com");  // replace with valid value
+                message.From = new MailAddress("info@proyectosweb.site");  // replace with valid value
                 message.Subject = "Contacto - VK Site";
                 message.Body = string.Format(body, model.FromName, model.FromPhone, model.FromEmail, model.FromCompany, model.Message);
                 message.IsBodyHtml = true;
 
                 using (var smtp = new SmtpClient())
                 {
-                    smtp.UseDefaultCredentials = false;
-                    var credential = new NetworkCredential
-                    {
-                        UserName = "******",  // replace with valid value
-                        Password = "******"  // replace with valid value
-                    };
-                    smtp.Credentials = credential;
-                    smtp.Host = "smtp.gmail.com";
-                    smtp.Port = 587;
-                    smtp.EnableSsl = true;
+                    smtp.UseDefaultCredentials = true;
+                    //var credential = new NetworkCredential
+                    //{
+                    //    UserName = "",  // replace with valid value
+                    //    Password = ""  // replace with valid value
+                    //};
+                    //smtp.Credentials = credential;
+                    smtp.Host = "localhost";
+                    smtp.Port = 465;
+                    smtp.EnableSsl = false;
 
                     await smtp.SendMailAsync(message);
                     return RedirectToAction("Index");
                 }
+
+
+
+
+
             }
             return View(model);
         }
